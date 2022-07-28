61ed3d41fe387263dadb772ca5a511952a0d5fe3
Simon-XzMC <simonlblakemore@gmail.com>
2022-07-28 20:59:47 +0100

    Start

---
+++ src/main/java/net/simonxz/prison/Prison.java
@@ -0,0 +1,14 @@
+package net.simonxz.prison;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Prison extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
